8ccc900246590abb0c16a5e2197d458443fa555a
Md Nakibul Hosen Nahid Nahid <nakibulh.aobs@gmail.com>
2022-04-19 13:14:32 +0600

    initiate project

---
+++ index.js
@@ -0,0 +1,33 @@
+/*
+*Title: Uptime Monitoring Application
+*Description: A RESTFUL API to monitor up or down time user defined links
+*Author: Md Nakibul Hosen Nahid (Origin: learn with Sumit)
+*Date: 19/04/22
+*
+*/
+// dependencies 
+const http = require('http');
+
+// app object - mudule scaffolding 
+const app = {};
+
+// configuration object 
+app.config = {
+    port: 3000
+};
+
+// create server 
+app.createServer = () => {
+    const server = http.createServer(app.handleReqRes);
+    server.listen(app.config.port,()=>{
+        console.log(`Listening to port ${app.config.port}`)
+    })
+}
+
+// handle Request and Response 
+app.handleReqRes = (req,res) => {
+    // response handle 
+    res.end('Hello Coders')
+}
+
+app.createServer();
