9f0806ab95b7c613178333331a12ebc1a53a7aef
Md Nakibul Hosen Nahid Nahid <nakibulh.aobs@gmail.com>
2022-04-20 15:39:16 +0600

    fix hashing

--- helpers/utilities.js
+++ helpers/utilities.js
@@ -26,16 +26,31 @@ utilities.parseJSON = (jsonString) => {
     return output
 }
 
-// Hashing 
-utilities.hash = (string) => {
-    if (typeof (string) === 'string' && string.length > 0) {
-        let hash = crypto
-            .createHmac('sha256', environments.secretKey)
-            .update("If you love node so much why don't you marry it?")
-            .digest('hex');
+// hash string
+utilities.hash = (str) => {
+    if (typeof str === 'string' && str.length > 0) {
+        console.log(environments, process.env.NODE_ENV);
+        const hash = crypto.createHmac('sha256', environments.secretKey).update(str).digest('hex');
         return hash;
     }
     return false;
+};
+
+// Token generation 
+utilities.createRandomString = (strLength) => {
+    let length = strLength;
+    length = typeof (strLength) === 'number' && strLength > 0 ? strLength : false;
+    if (length) {
+        let possibleCharacters = 'abcdefghjklmnopqrstuvwxyz1234567890';
+        let output = '';
+        for (let i = 1; i <= length; i += 1) {
+            const randomCharacter = possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
+            output += randomCharacter;
+        }
+        return output;
+    } else {
+        return false;
+    }
 }
 // export module 
 module.exports = utilities;
