556a61e5755bf4900b91ad1e9d6c0aa0e34eaf71
Md Nakibul Hosen Nahid Nahid <nakibulh.aobs@gmail.com>
2022-04-20 15:40:11 +0600

    add token handler route

--- routes.js
+++ routes.js
@@ -9,9 +9,11 @@
 // dependencies 
 const { sampleHandler } = require('./handlers/routeHandlers/smapleHandler');
 const { userHandler } = require('./handlers/routeHandlers/userHandler');
+const { tokenHandler } = require('./handlers/routeHandlers/tokenHandler');
 const routes = {
     'sample': sampleHandler,
-    'user': userHandler
+    'user': userHandler,
+    'token': tokenHandler
 }
 
 module.exports = routes;
