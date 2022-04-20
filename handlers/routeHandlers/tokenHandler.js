20b1bf20a17315a5e3f8c855dcfdbdb655f53ed2
Md Nakibul Hosen Nahid Nahid <nakibulh.aobs@gmail.com>
2022-04-20 15:52:49 +0600

    add get token api

--- handlers/routeHandlers/tokenHandler.js
+++ handlers/routeHandlers/tokenHandler.js
@@ -84,7 +84,26 @@ handler._token.post = (requestProperties, callback) => {
     }
 }
 handler._token.get = (requestProperties, callback) => {
+    // check the phone number is valid or not 
+    const id = typeof (requestProperties.queryStringObject.id) === 'string' && requestProperties.queryStringObject.id.trim().length === 20 ? requestProperties.queryStringObject.id : false;
 
+    if (id) {
+        data.read('tokens', id, (err, utoken) => {
+            const token = { ...parseJSON(utoken) }
+            if (!err && token) {
+                callback(200, token)
+            } else {
+                callback(404, {
+                    'message': 'Requested token does not exist'
+                })
+            }
+        })
+    } else {
+        callback(400, {
+            'message': 'Enter valid token id',
+
+        })
+    }
 }
 handler._token.put = (requestProperties, callback) => {
 
