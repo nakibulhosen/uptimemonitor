a95e6dd26066aae68585562765170080e800bb1e
Md Nakibul Hosen Nahid Nahid <nakibulh.aobs@gmail.com>
2022-04-20 10:28:01 +0600

    implemented update and delete functionality

--- handlers/routeHandlers/userHandler.js
+++ handlers/routeHandlers/userHandler.js
@@ -8,6 +8,7 @@
 // dependencies 
 const data = require('../../lib/data');
 const { hash } = require('../../helpers/utilities')
+const { parseJSON } = require('../../helpers/utilities')
 
 
 // module scaffolding 
@@ -72,14 +73,106 @@ handler._user.post = (requestProperties, callback) => {
 
 }
 handler._user.get = (requestProperties, callback) => {
-    callback(200)
+    // check the phone number is valid or not 
+    const phone = typeof (requestProperties.queryStringObject.phone) === 'string' && requestProperties.queryStringObject.phone.trim().length === 11 ? requestProperties.queryStringObject.phone : false;
+
+    if (phone) {
+        data.read('users', phone, (err, u) => {
+            const user = { ...parseJSON(u) }
+            if (!err && user) {
+                delete user.password;
+                callback(200, user)
+            } else {
+                callback(404, {
+                    'message': 'Requested user does not exist'
+                })
+            }
+        })
+    } else {
+        callback(400, {
+            'message': 'Enter valid phone number',
+
+        })
+    }
 }
 handler._user.put = (requestProperties, callback) => {
+    const firstName = typeof (requestProperties.body.firstName) === 'string' && requestProperties.body.firstName.trim().length > 0 ? requestProperties.body.firstName : false;
+    const lastName = typeof (requestProperties.body.lastName) === 'string' && requestProperties.body.lastName.trim().length > 0 ? requestProperties.body.lastName : false;
+    const phone = typeof (requestProperties.body.phone) === 'string' && requestProperties.body.phone.trim().length === 11 ? requestProperties.body.phone : false;
+    const password = typeof (requestProperties.body.password) === 'string' && requestProperties.body.password.trim().length > 0 ? requestProperties.body.password : false;
+    if (phone) {
+        if (firstName || lastName || password) {
+            //Lookup for phone number
+            data.read('users', phone, (err, uData) => {
+                const userData = { ...parseJSON(uData) };
+                if (!err && userData) {
+                    if (firstName) {
+                        userData.firstName = firstName;
+                    }
+                    if (lastName) {
+                        userData.lastName = lastName
+                    } if (password) {
+                        userData.password = hash(password)
+                    }
 
+                    // Store to database 
+
+                    data.update('users', phone, userData, (err) => {
+                        if (!err) {
+                            callback(200, {
+                                'message': 'User updated successfully'
+                            })
+                        } else {
+                            callback(500, {
+                                message: 'There was a problem in server side'
+                            })
+                        }
+                    })
+                } else {
+                    callback(404, {
+                        'message': 'User does not exist'
+                    })
+                }
+            })
+        } else {
+            callback(400, {
+                'message': 'Nothing to change'
+            })
+        }
+    } else {
+        callback(400, {
+            'message': 'Error Invalid phone number'
+        })
+    }
 }
-
 handler._user.delete = (requestProperties, callback) => {
-
+    // check the phone number is valid or not 
+    const phone = typeof (requestProperties.queryStringObject.phone) === 'string' && requestProperties.queryStringObject.phone.trim().length === 11 ? requestProperties.queryStringObject.phone : false;
+    if (phone) {
+        data.read('users', phone, (err, userData) => {
+            if (!err) {
+                data.delete('users', phone, (err) => {
+                    if (!err) {
+                        callback(200, {
+                            'message': 'User deleted successfully'
+                        })
+                    } else {
+                        callback(500, {
+                            'message': 'There was a server side error'
+                        })
+                    }
+                })
+            } else {
+                callback(500, {
+                    'message': 'There was a server side error'
+                })
+            }
+        })
+    } else {
+        callback(400, {
+            'message': 'Invalid phone number'
+        })
+    }
 }
 
 module.exports = handler;
